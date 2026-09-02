5959689da5d1ce7e8ef19bb806ea94d129f2a6f0
popaciprianaurelian-pixel <popa.ciprian.aurelian@gmail.com>
2026-09-02 21:18:29 +0300

    Update MainActivity.java

--- app/app/src/main/app/src/main/res/layout/app/src/main/java/com/example/calculatorore/MainActivity.java
+++ app/app/src/main/app/src/main/res/layout/app/src/main/java/com/example/calculatorore/MainActivity.java
@@ -1,13 +1,13 @@
 package com.example.calculatorore;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     private EditText etStart, etEnd;
     private TextView tvResult;
@@ -61,3 +61,4 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 }
+
